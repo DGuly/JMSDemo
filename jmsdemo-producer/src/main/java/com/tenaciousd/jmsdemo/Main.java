e28c16ab69e6fbd9103f6f5460d00ccfd996e9e2
ytalashko <talashkoy@gmail.com>
2015-10-30 02:55:23 +0200

    Add message producer

--- jmsdemo-producer/src/main/java/com/tenaciousd/jmsdemo/Main.java
+++ jmsdemo-producer/src/main/java/com/tenaciousd/jmsdemo/Main.java
@@ -1,7 +1,25 @@
 package com.tenaciousd.jmsdemo;
 
-/**
- * Producer main.
- */
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
 public class Main {
+
+  public static void main(String[] args) throws InterruptedException, JMSException {
+    try (Connection connection = new ActiveMQConnectionFactory("tcp://localhost:61616").createConnection()) {
+      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+      Destination destination = session.createQueue("test-queue");
+      MessageProducer producer = session.createProducer(destination);
+      producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+      int number = 1;
+      while (true) {
+        producer.send(session.createTextMessage("Test message number: " + number++));
+        Thread.sleep(500);
+      }
+    } catch (Exception e) {
+      System.out.println("Failed to send message: " + e.getMessage());
+    }
+  }
 }
